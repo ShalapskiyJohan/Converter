9bfea542b6b105f755de0ce93fd56f9db42d9c6d
Пользователь <ivanshalapskiy2409@gmail.com>
2025-03-06 20:17:56 +0200

    develop

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,27 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static final double CONV_K = 2.20462;
+    private static final double CONV_K = 2.20463;
 
-    public static void main(String[] args) { System.out.println("Converter APP.");
+    public static void main(String[] args) {
+        System.out.println("Converterw App.");
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Results is " + pounds + "pounds");
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is" + pounds + " pounds and "
+                + kilos + "kgs.");
+
     }
-    public static double convKgsToPounds(double kgs) {
+
+    private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
 }
